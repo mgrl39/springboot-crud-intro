0a91ebf7b3a6f3bf1a4ec90d94df016966b9a314
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-13 02:29:01 +0200

    code base

---
+++ src/main/java/net/xeill/elpuig/springbootcrudintro/SpringbootCrudIntroApplication.java
@@ -0,0 +1,13 @@
+package net.xeill.elpuig.springbootcrudintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCrudIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/xeill/elpuig/springbootcrudintro/SpringbootCrudIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xeill.elpuig.springbootcrudintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
